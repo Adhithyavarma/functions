7d5260e7224e71dd8502cb886a2eb8ca86482757
Adhithyavarma <111036380+Adhithyavarma@users.noreply.github.com>
2022-08-19 20:41:59 +0530

    Add files via upload

---
+++ fn assignment.js
@@ -0,0 +1,82 @@
+
+ 
+function add(a,b){
+    let A=a+b
+    return A  
+}
+let f=add(4,4)
+console.log(f)
+
+function mul2(a,b){
+    let A=a*b
+    return A
+}
+let g=mul2(7,9)
+console.log(g)
+
+function mul3(a,b,c){
+    let A=a*b*c
+    return A
+}
+let h=mul3(3,4,5)
+console.log(h)
+
+function cube(a){
+    let A=a*a*a
+    return A
+}
+let i=cube(5)
+console.log(i)
+
+function volofcube(a){
+    let A=a*a*a
+    return A
+}
+let j=volofcube(2)
+console.log(j)
+
+function areaofsquare(a){
+    let A=a*a
+   return A
+}
+let k=areaofsquare(8)
+console.log (k)
+
+function areaofrectangle(l,b){
+    let A=l*b
+    return A
+}
+let m=areaofrectangle(4,5)
+console.log (m)
+
+function diagonalofsquare(a){
+    let A=1.4*a
+    return A
+}
+let n=diagonalofsquare(4,5)
+console.log (n)
+
+function perimeterofsquare(a){
+    let A=4*a
+    return A
+}
+let o=perimeterofsquare(4)
+console.log (o)
+
+function si(p,r,t){
+    let A=(p*r*t)/100
+    return A
+}
+let q=si(4,5,6)
+console.log (q)
+
+function ci(p,r,n,t){
+    let A=p*(Math.pow((1+(r/n)),(n*t)))
+    
+
+    return u
+}
+let s=si(2000,0.8,12,5)
+console.log (s)
+
+
